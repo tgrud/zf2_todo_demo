42c37f712178675e6cd72965b8eec581e66000f0
tgrud <tgrud@users.noreply.github.com>
2015-08-08 11:46:24 -0500

    Add TodoController with empty CRUD methods and base CRUD Todo view files

--- module/Todo/config/module.config.php
+++ module/Todo/config/module.config.php
@@ -5,6 +5,24 @@ return array(
             'Todo\Controller\Todo' => 'Todo\Controller\TodoController',
         ),
     ),
+    'router' => array(
+        'routes' => array(
+            'todo' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/todo[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Todo\Controller\Todo',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
     'view_manager' => array(
         'template_path_stack' => array(
             'todo' => __DIR__ . '/../view',
